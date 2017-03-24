05da232b82199cb364bafb470a2798822658bc3e
gaoyx <melo@gaoyuexiang>
2017-03-24 16:27:47 +0800

    add test for should return "Buzz"

--- spec/fizzBuzz/fizzBuzzSpec.js
+++ spec/fizzBuzz/fizzBuzzSpec.js
@@ -1,8 +1,13 @@
 describe('FizzBuzz', function () {
   let fizzBuzz = require('../../lib/fizzBuzz/fizzBuzz.js')
-  it('should return "Fizz" when given 3', function () {
+  it('should return "Fizz" when given number multiple of 3', function () {
     let fizz = fizzBuzz(3)
 
     expect(fizz).toEqual('Fizz')
+  })
+  it('should return "Buzz" when given number multiple of 5', function() {
+    let buzz = fizzBuzz(5)
+
+    expect(buzz).toEqual('Buzz')
   })
 })
