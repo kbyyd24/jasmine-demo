da3ae6daa2416dfb6a38b2112090acdc406d969d
gaoyx <melo@gaoyuexiang.cn>
2017-03-24 15:18:22 +0800

    add test for minus

--- spec/math/mathSpec.js
+++ spec/math/mathSpec.js
@@ -1,7 +1,12 @@
 describe('math', function () {
     var add = require('../../lib/math/math.js')
-    it('should return sum of two operands', function () {
+    it('should return the sum of two operands', function () {
         var sum = add(1, 2)
         expect(sum).toEqual(3)
+    })
+
+    it('should return subtraction of two operands', function() {
+        var subtraction = minus(3, 2)
+        expect(subtraction).toEqual(1)
     })
 })
