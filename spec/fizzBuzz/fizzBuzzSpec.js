6f2fb33cdb71ab734247f2a26ad87487f888f307
gaoyx <melo@gaoyuexiang.cn>
2017-03-24 15:53:25 +0800

    update format

--- spec/fizzBuzz/fizzBuzzSpec.js
+++ spec/fizzBuzz/fizzBuzzSpec.js
@@ -2,6 +2,7 @@ describe('FizzBuzz', function () {
   let fizzBuzz = require('../../lib/fizzBuzz.js')
   it('should return Fizz when given 3', function () {
     let fizz = fizzBuzz(3)
+
     expect(fizz).toEqual('Fizz')
   })
 })
