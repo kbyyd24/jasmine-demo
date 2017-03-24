a2e06c2d3c070e685cee7f679589dfc32b0912df
gaoyx <melo@gaoyuexiang.cn>
2017-03-24 15:20:58 +0800

    format

--- spec/math/mathSpec.js
+++ spec/math/mathSpec.js
@@ -1,12 +1,12 @@
 describe('math', function () {
-    var math = require('../../lib/math/math.js')
-    it('should return the sum of two operands', function () {
-        var sum = math.add(1, 2)
-        expect(sum).toEqual(3)
-    })
+  var math = require('../../lib/math/math.js')
+  it('should return the sum of two operands', function () {
+    var sum = math.add(1, 2)
+    expect(sum).toEqual(3)
+  })
 
-    it('should return subtraction of two operands', function() {
-        var subtraction = math.minus(3, 2)
-        expect(subtraction).toEqual(1)
-    })
+  it('should return subtraction of two operands', function() {
+    var subtraction = math.minus(3, 2)
+    expect(subtraction).toEqual(1)
+  })
 })
